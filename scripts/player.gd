99d940b2dff9eedb9eaa69ce6c72011ac7a196a9
Ricky Xu <89417286+terminator0521@users.noreply.github.com>
2026-09-13 21:24:49 -0400

    add player walking animation

---
+++ scripts/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# right to left movement
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= SPEED * delta
+		$AnimatedSprite2D.flip_h = 1
+		$AnimatedSprite2D.play("walking")
+	elif Input.is_key_pressed(KEY_RIGHT):
+		$AnimatedSprite2D.flip_h = 0
+		$AnimatedSprite2D.play("walking")
+		velocity.x += SPEED * delta
+	else:
+		$AnimatedSprite2D.play("default")
+
+	move_and_slide()
+	pass
