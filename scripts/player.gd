3d82f8ff0295faca03599c167857170e23802203
Ricky Xu <89417286+terminator0521@users.noreply.github.com>
2026-09-14 17:11:14 -0400

    remove rocket multimeshinstance and sliding from player

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,15 +10,14 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	# right to left movement
 	if Input.is_key_pressed(KEY_LEFT):
-		velocity.x -= SPEED * delta
+		position.x -= SPEED * delta
 		$AnimatedSprite2D.flip_h = 1
 		$AnimatedSprite2D.play("walking")
 	elif Input.is_key_pressed(KEY_RIGHT):
 		$AnimatedSprite2D.flip_h = 0
 		$AnimatedSprite2D.play("walking")
-		velocity.x += SPEED * delta
+		position.x += SPEED * delta
 	else:
 		$AnimatedSprite2D.play("default")
 
-	move_and_slide()
 	pass
