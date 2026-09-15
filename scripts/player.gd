c7a8090ba84abd412d16df358efa6af61233679d
Ricky Xu <89417286+terminator0521@users.noreply.github.com>
2026-09-14 21:29:44 -0400

    remake rocket with single node instance

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,36 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+var h_direction: float
 
 func _ready() -> void:
 	pass
 
-func _physics_process(delta: float) -> void:
-	# right to left movement
-	if Input.is_key_pressed(KEY_LEFT):
-		position.x -= SPEED * delta
-		$AnimatedSprite2D.flip_h = 1
-		$AnimatedSprite2D.play("walking")
-	elif Input.is_key_pressed(KEY_RIGHT):
-		$AnimatedSprite2D.flip_h = 0
-		$AnimatedSprite2D.play("walking")
-		position.x += SPEED * delta
-	else:
-		$AnimatedSprite2D.play("default")
+func _process(delta: float) -> void:
+	if h_direction != 0:
+		$AnimatedSprite2D.flip_h = (h_direction < 0)
 
+	match h_direction:
+		1.0, -1.0:
+			if $AnimatedSprite2D.animation != "walking":
+				$AnimatedSprite2D.play("walking")
+		0.0:
+			if $AnimatedSprite2D.animation != "default":
+				$AnimatedSprite2D.play("default")
+	pass
+
+func _physics_process(delta: float) -> void:
+	# right to left direction
+	if Input.is_key_pressed(KEY_LEFT):
+		h_direction = -1
+	elif Input.is_key_pressed(KEY_RIGHT):
+		h_direction = 1
+	else:
+		h_direction = 0
+	
+	# move
+	velocity.x = h_direction * SPEED
+	move_and_slide()
 	pass
