039632479212b71a56c6c9b6909d731924f68fe8
Ricky Xu <89417286+terminator0521@users.noreply.github.com>
2026-09-14 22:36:34 -0400

    add area2d nodes for player and rocket

--- scripts/rocket_manager.gd
+++ scripts/rocket_manager.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
+var rockets: Array[CharacterBody2D]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	
 	pass # Replace with function body.
 
 
